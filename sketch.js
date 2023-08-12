995a020230c72f6f6bbbf7ac6b1c2eed6a1993de
kitilibup <43076707+kitilibup@users.noreply.github.com>
2023-08-12 21:49:24 +0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let font;
 let responsiveText = 250;
 let vehicles = [];
 function preload () {
-  font = loadFont('../fonts/caveat-regular.otf');
+  font = loadFont('fonts/Caveat-Regular.otf')
 }
 
 function setup() {
